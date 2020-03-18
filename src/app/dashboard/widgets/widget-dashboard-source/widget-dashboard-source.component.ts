3a9110aa4cd358acac43d25aff0954775cc74feb
batouche-dev <azedine.batouche@outlook.com>
2020-03-18 13:51:10 +0100

    feat:  update widgets

--- src/app/dashboard/widgets/widget-dashboard-source/widget-dashboard-source.component.ts
+++ src/app/dashboard/widgets/widget-dashboard-source/widget-dashboard-source.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-widget-dashboard-source',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./widget-dashboard-source.component.scss']
 })
 export class WidgetDashboardSourceComponent implements OnInit {
-  constructor() {}
+  protected test= 'https://github.com/batouche-dev/dashboard'
+  constructor(private router: Router) {}
 
+  download(){
+    this.router.navigateByUrl('https://codeload.github.com/batouche-dev/dashboard/zip/1.0.0');
+  }
   ngOnInit() {}
 }
